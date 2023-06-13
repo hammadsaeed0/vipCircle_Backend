94b86d607d6f02d8757208707d68588b3291e263
hammad <hammadsaeed083@gmail.com>
2023-06-14 03:48:32 +0500

    Today Work

--- controller/userController.js
+++ controller/userController.js
@@ -269,3 +269,14 @@ export const ShowProfile = catchAsyncError(async (req, res, next) => {
   });
 });
 
+// Show Single Profile
+export const ShowSingleProfile = catchAsyncError(async (req, res, next) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+  if (!user) return res.status(404).json({ message: "User not found" });
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { AddPhoneNumber, ShowProfile,  WhoIam, Interests, PersonlDetail, Hobbies, LikeProfile, LikedProfile, DeleteProfile ,uploadImage , AddProfileDetail , uploadVideo} from "../controller/userController.js";
+import { AddPhoneNumber, ShowProfile,  WhoIam, Interests, PersonlDetail, Hobbies, LikeProfile, LikedProfile, DeleteProfile ,uploadImage , AddProfileDetail , uploadVideo, ShowSingleProfile} from "../controller/userController.js";
 import multer from 'multer';
 const upload = multer({ dest: 'uploads/' });
 
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.route("/AddPhoneNumber").post(AddPhoneNumber);
 router.route("/ShowProfile").get(ShowProfile);
+router.route("/ShowSingleProfile/:id").post(ShowSingleProfile);
 router.route("/AddProfileDetail/:id").post(AddProfileDetail);
 router.route("/WhoIam/:id").post(WhoIam);
 router.route("/uploadImage" , upload.array('avatars')).post(uploadImage);
