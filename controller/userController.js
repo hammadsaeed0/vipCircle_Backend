633d701e22b5b64cbc37f13109c4a3bfb297fcf0
hammadsaeed <hammadsaeed083@gmail.com>
2023-07-08 00:20:06 +0500

    Check EMail

--- controller/userController.js
+++ controller/userController.js
@@ -28,13 +28,13 @@ export const AddPhoneNumber = catchAsyncError(async (req, res, next) => {
   sendToken(res, newUser, "User Register", 201);
 });
 
-// Add User Phone Number
+// Add User Email
 export const AddEmail = catchAsyncError(async (req, res, next) => {
   const { email } = req.body;
   const existingUser = await User.findOne({ email });
   console.log(existingUser);
   if (existingUser) {
-    return res.status(400).json({ message: "Email already exists" });
+    return res.status(400).json({ success: "false",  message: "Email already exists" });
   }
 
   const newUser = await User.create({ email });
